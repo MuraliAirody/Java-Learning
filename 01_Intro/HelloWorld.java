72cdb1e5d734560bf9a504031b311a1b0abc3bc2
MuraliAirody <muraliairody@gmail.com>
2023-11-21 21:09:43 +0530

    created Hello World

---
+++ 01_Intro/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World!!"+ args[2]);
+    }
+}
+
+
+//arguments inside the main are command line arguments "java HelloWorld 15 20 150"
+
+//main method should be present in the public class
+//the name of the file should be same as public class (its a naming convention, some IDE follows this rule) 
