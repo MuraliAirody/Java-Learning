b7b0b80c6c4a258df21d114e34851433621e8891
muraliairody <muraliairody@gmail.com>
2026-01-19 10:46:01 +0530

    insert first into doubly ll

---
+++ LinkedList/doublyLL/Main.java
@@ -0,0 +1,13 @@
+package LinkedList.doublyLL;
+
+public class Main {
+    public static void main(String[] args) {
+        DLL list = new DLL();
+
+        list.insertFirst(20);
+        list.insertFirst(40);
+        list.insertFirst(10);
+
+        list.display();
+    }
+}
